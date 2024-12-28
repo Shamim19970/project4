24d3d2c86e5c033b71d1a248d2fcdd0cd60b66f7
Shamim <osamim395@gmail.com>
2024-12-28 20:12:35 +0600

    first commit

---
+++ public/index.php
@@ -0,0 +1,46 @@
+<?php
+require_once "./../app/classes/VehicleManager.php";
+
+$vehicleManager = new VehicleManager('', '', '', '');
+$vehicles = $vehicleManager->getVehicles();
+
+
+
+include './views/header.php';
+
+
+
+?>
+
+
+<div class="container my-4">
+    <h1>Vehicle Listing</h1>
+    <a href="./../public/views/add.php" class="btn btn-success mb-4">Add Vehicle</a>
+    <div class="row">
+        <!-- Loop Go here -->
+         <?php foreach($vehicles as $id=>$vehicle): ?>
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="<?= $vehicle['image'] ?>" class="card-img-top" style="height: 200px; object-fit: cover;">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $vehicle['name'] ?></h5> 
+                         <p class="card-text">Type: <?= $vehicle['type'] ?></p>
+                        <p class="card-text">Price: $<?= $vehicle['price'] ?></p>
+                        <a href="./views/edit.php?id=<?= $id ?>" class="btn btn-primary">Edit</a>
+                        <a href="./views/delete.php?id=<?= $id ?>" class="btn btn-danger">Delete</a>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        <!-- Loop ends here -->
+    </div>
+</div>
+
+</body>
+</html>
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Shamim19970/project4.git
+git push -u origin main
